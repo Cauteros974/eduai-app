b6e85f47cb12226256747036169e0f5e8b7fdfd6
Cauteros974 <pusik6666@ukr.net>
2025-10-04 19:35:02 +0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,3 +5,12 @@ const api = axios.create({
     baseURL: 'http://localhost:5000/api',
 });
 
+api.interceptors.request.use((config) => {
+    const token = useUserStore.getState().token;
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
