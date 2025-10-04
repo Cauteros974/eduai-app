c0813a515bfb4d5a8527e8c9372cf191007cde75
Cauteros974 <pusik6666@ukr.net>
2025-10-04 19:17:46 +0300

    Create userStore.ts

---
+++ src/store/userStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+
+interface User {
+    id: string, 
+    email: string,
+    name: string,
+}
+
+interface UserState {
+    user: User | null;
+    token: string | null;
+    isloading: boolean;
+    login: (userData: User, token: string) => void;
+    logout: () => void;
+}
+
+export const useUserStore = create<UserState((set) => (
+    user: null,
+    token: null,
+    isloading: true,
+    login: (userData, token) => set({ user: userData, token, isLoading: false }),
+))
