d61e9f74590abfac2b9de94351a9596f36c29ab3
Cauteros974 <pusik6666@ukr.net>
2025-10-07 20:26:29 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { LogiPage } from './pages/LoginPage';
+import { LoginPage } from './pages/LoginPage';
 import { HomePage } from './pages/HomePage';
 import { CoursePage } from './pages/CoursePage';
 import { useUserStore } from './store/userStore';
@@ -8,3 +8,31 @@ const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const user = useUserStore((state) => state.user);
   return user ? children : <Navigate to="/login" />;
 };
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/course/:courseId"
+          element={
+            <PrivateRoute>
+              <CoursePage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
