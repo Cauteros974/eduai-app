6439e02e7a72d801d20312e1c07ac99ceb36220f
Cauteros974 <pusik6666@ukr.net>
2025-10-07 21:05:39 +0300

    Create Button.tsx

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,8 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+
+const buttonStyle: React.CSSProperties = {
+    padding: '10px 20px',
+    fontSize: '16px',
+    fontWeight: 300,
+};
