47f04a8f00fc3aafcd8e15d392f34b8624b1a4fb
Cauteros974 <pusik6666@ukr.net>
2025-10-08 18:10:39 +0300

    Update Input.tsx

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -14,3 +14,23 @@ const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
     e.target.style.borderColor = 'var(--primary-color)';
     e.target.style.boxShadow = '0 0 0 3px rgba(99, 102, 241, 0.2)';
 };
+
+const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.target.style.borderColor = 'var(--border-color)';
+    e.target.style.boxShadow = 'none';
+};
+
+export const Input = React.forwardRef<
+    HTMLInputElement,
+    React.InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+    return(
+        <input
+            ref={ref}
+            style={inputStyle}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            {...props}
+        />
+    );
+});
