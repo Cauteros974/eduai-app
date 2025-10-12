f37a303a1ac21e0ccca578627cfc023f4ce9b2e1
Cauteros974 <pusik6666@ukr.net>
2025-10-12 17:01:40 +0300

    Update HomePage.tsx

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import { CourseCard } from "../features/Course/CourseCard";
+
+const mockCourses = [
+    { id: 1, title: 'Introduction to React', description: 'Basics of modern web development.', imageUrl: 'https://placehold.co/600x400/6366f1/FFF?text=React'},
+    { id: 2, title: 'TypeScript for professionals', description: 'Learn static typing.', imageUrl: 'https://placehold.co/600x400/3498db/FFF?text=TS'},
+    { id: 3, title: 'Node.js and Express', description: 'Creation of powerful backend services.', imageUrl: 'https://placehold.co/600x400/2ecc71/FFF?text=Node'},
+]
