53a907154bacc189bd830f26dff346ed367eb5d8
Cauteros974 <pusik6666@ukr.net>
2025-10-12 16:21:18 +0300

    Update CourseCard.tsx

---
+++ src/features/Course/CourseCard.tsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+
+const cardStyle: React.CSSProperties = {
+    background: 'var(--card-background)',
+    borderRadius: 'var(--border-radius)',
+    boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+    cursor: 'pointer',
+    overflow: 'hidden',
+};
+
+const imageStyle: React.CSSProperties = {
+    width: '100%',
+    height: '160px',
+    objectFit: 'cover',
+};
+  
