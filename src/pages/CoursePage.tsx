ce407a8f475b1159e4df7ba3d6889af08e1f919e
Cauteros974 <pusik6666@ukr.net>
2025-10-16 19:27:58 +0300

    Update CoursePage.tsx

--- src/pages/CoursePage.tsx
+++ src/pages/CoursePage.tsx
@@ -32,7 +32,7 @@ export const CoursePage = () => {
                     </p>
                 </div>
                 {!showQuiz && (
-                    <div style={{marginTop: '30px', width: '200px', position: 'right'}}>
+                    <div style={{marginTop: '30px', width: '200px', position: 'fixed'}}>
                         <Button onClick={() => setShowQuiz(true)}>Take the test</Button>
                     </div>
                 )}
