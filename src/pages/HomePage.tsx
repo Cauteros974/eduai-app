e3689bf1399ed758b32de5bdd04de69432015411
Cauteros974 <pusik6666@ukr.net>
2025-10-17 16:42:10 +0300

    commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,44 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import { CourseCard } from "../features/Course/CourseCard";
-import styles from './HomePage.module.css';
+import styles from "./HomePage.module.css";
 
 const mockCourses = [
-    { id: 1, title: 'Introduction to React', description: 'Basics of modern web development.', imageUrl: "/images/react.png"},
-    { id: 2, title: 'TypeScript for professionals', description: 'Learn static typing.', imageUrl: '/images/Typescript.png'},
-    { id: 3, title: 'Node.js and Express', description: 'Creation of powerful backend services.', imageUrl: '/images/Node.js.png'},
-    { id: 4, title: 'Angular', description: 'Build modern apps', imageUrl: '/images/Angular_gradient.png'},
+  { id: 1, title: "Introduction to React", description: "Basics of modern web development.", imageUrl: "/images/react.png" },
+  { id: 2, title: "TypeScript for professionals", description: "Learn static typing.", imageUrl: "/images/Typescript.png" },
+  { id: 3, title: "Node.js and Express", description: "Creation of powerful backend services.", imageUrl: "/images/Node.js.png" },
+  { id: 4, title: "Angular", description: "Build modern apps.", imageUrl: "/images/Angular_gradient.png" },
 ];
 
-const gridStyle: React.CSSProperties = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
-    gap: '24px',
-};
-
 export const HomePage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleCourseClick = ( courseId: number) => {
-        console.log('Click! I am trying to switch to a course with ID:', courseId);
-        navigate(`/course/${courseId}`)
-    };
+  const handleCourseClick = (courseId: number) => {
+    console.log("Click! I am trying to switch to a course with ID:", courseId);
+    navigate(`/course/${courseId}`);
+  };
 
-    return (
-      <>
-      <div className={styles.page}>
-        <h1 style={{ marginBottom: '32px' }}>Available courses</h1>
-        <div style={gridStyle}>
-          {mockCourses.map((course) => (
+  return (
+    <div className={styles.page}>
+      <h1 className={styles.title}>Available courses</h1>
+
+      <div className={styles.grid}>
+        {mockCourses.map((course) => (
+          <div key={course.id} onClick={() => handleCourseClick(course.id)}>
             <CourseCard
-              key={course.id}
               title={course.title}
               description={course.description}
               imageUrl={course.imageUrl}
               onClick={() => handleCourseClick(course.id)}
             />
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-      </>
-    );
+    </div>
+  );
 };
